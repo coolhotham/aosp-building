1adb29d928ae98d1c99f7c54ec01ba64f1e785f2
coolhotham <80941122+coolhotham@users.noreply.github.com>
2021-03-19 11:32:00 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,41 +14,40 @@ function tg_sendFile() {
 curl -F chat_id=$CHAT_ID -F document=@${1} -F parse_mode=markdown https://api.telegram.org/bot$BOT_TOKEN/sendDocument
 }
 
-MANIFEST=git://github.com/crdroidandroid/android.git
-BRANCH=11.0
+MANIFEST=git://github.com/AOSiP/platform_manifest.git
+BRANCH=eleven
 
 mkdir -p /tmp/rom
 cd /tmp/rom
 
 # Repo init command, that -device,-mips,-darwin,-notdefault part will save you more time and storage to sync, add more according to your rom and choice. Optimization is welcomed! Let's make it quit, and with depth=1 so that no unnecessary things.
-repo init --no-repo-verify --depth=1 -u "$MANIFEST" -b "$BRANCH" -g default,-device,-mips,-darwin,-notdefault
+repo init -u "$MANIFEST" -b "$BRANCH"
 
 tg_sendText "Downloading sources"
 # Sync source with -q, no need unnecessary messages, you can remove -q if want! try with -j30 first, if fails, it will try again with -j8
-repo sync -c -q --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+repo sync --force-sync --no-tags --no-clone-bundle -j30 || repo sync --force-sync --no-tags --no-clone-bundle -j8
 rm -rf .repo
 
 # Sync device tree and stuffs
-git clone https://github.com/Gabriel260/android_device_samsung_a10-common -b crdroid-arm32 device/samsung
-git clone https://github.com/Gabriel260/proprietary_vendor_samsung_a10-common -b lineage-18.1-arm32 vendor/samsung
-git clone --depth=1 https://github.com/geckyn/android_kernel_samsung_exynos7885 kernel/samsung/exynos7885
-git clone https://github.com/Gabriel260/android_hardware_samsung-2 hardware/samsung
+git clone -b arrow-11.0 https://github.com/coolhotham/device_lav.git device/xiaomi/lavender
+git clone -b arrow-11.0 https://github.com/coolhotham/vendor_lav.git vendor/xiaomi/lavender
+git clone -b Hmp https://github.com/NotZeetaa/nexus_kernel_lavender.git kernel/xiaomi/lavender
 
 # Normal build steps
-. build/envsetup.sh
+source build/envsetup.sh
 export USE_CCACHE=0
-lunch lineage_a10-userdebug
+lunch aosip_lavender-userdebug
 
 # upload function for uploading rom zip file! I don't want unwanted builds in my google drive haha!
 up(){
 	curl --upload-file $1 https://transfer.sh/ | tee download.txt
 }
 tg_sendText "Building"
-mka api-stubs-docs
-mka system-api-stubs-docs
-mka test-api-stubs-docs
-mka bacon -j16
-up out/target/product/a10/*.zip
+#mka api-stubs-docs
+#mka system-api-stubs-docs
+#mka test-api-stubs-docs
+time m kronic
+up out/target/product/lavender/*.zip
 tg_sendText "download.txt"
-up out/target/product/a10/*.json
+up out/target/product/lavender/*.json
 tg_sendText "download.txt"
